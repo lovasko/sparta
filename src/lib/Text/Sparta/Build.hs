3e1904b66e375fe9c8ceeed49cc8189c2433d885
Daniel Lovasko <daniel.lovasko@gmail.com>
2016-11-11 20:32:33 +0000

    sorting of columns during the build

--- src/lib/Text/Sparta/Build.hs
+++ src/lib/Text/Sparta/Build.hs
@@ -2,11 +2,14 @@ module Text.Sparta.Build
 ( build
 ) where
 
+import Data.List
+
+import qualified Data.Foldable as F
 import qualified Data.Sequence as S
 import qualified Data.Text as T
 
-import Token
-import Types
+import Text.Sparta.Token
+import Text.Sparta.Types
 
 -- | Build a table from textual data.
 build :: [[T.Text]]  -- ^ cells
@@ -14,49 +17,39 @@ build :: [[T.Text]]  -- ^ cells
 build []   = Nothing
 build [[]] = Nothing
 build cells
-  | same (map length cells) = build'
+  | same (map length cells) = Just cols3
   | otherwise               = Nothing
   where
-    build'    = Just $ Table $ foldr rowAppend columns cells
-    rowAppend = flip $ zipWith columnAppend
-    colcnt    = length $ head cells
-    columns   = replicate colcnt (Column S.empty 0)
+    cols1 = replicate (length $ head cells) S.empty
+    cols2 = foldr (zipWith columnAppend) cols1 cells
+    cols3 = compSorted $ zip [1..] cols2
 
-ordering :: [Columns]
-         -> Table
-ordering cols = Table ordCols
-  where
-    ordCols = sortBy compOrd numCols
-    numCols = zip [1..] cols
-    compOrd (_, Column _ c1) (_, Column _ c2) = compare c1 c2
+-- | Return columns sorted by the complexity of each.
+compSorted :: [(Int, Column)] -- ^ columns
+           -> [(Int, Column)] -- ^ sorted columns
+compSorted = sortBy (\c1 c2 -> compare (columnRate c1) (columnRate c2))
+  where columnRate = F.sum . fmap rate . snd
 
 -- | Parse cell content and append it to a column.
-columnAppend :: Column -- ^ old column
-             -> T.Text -- ^ cell content
+columnAppend :: T.Text -- ^ cell content
+             -> Column -- ^ old column
              -> Column -- ^ new column
-columnAppend (Column cells comp) text = Column newCells newComp
-  where
-    tokens   = tokenize text
-    newComp  = comp + complexity tokens
-    newCells = cells S.|> tokens
+columnAppend text col = col S.|> (tokenize text)
 
 -- | Rate the complexity of a set of tokens. The higher the rate, the more
 -- complex the tokens are. Constants used in this function are rather
 -- arbitrary and should be reasoned about in the future.
-complexity :: [Token] -- ^ tokens
-           -> Integer -- ^ complexity
-complexity [] = 0
-complexity (t:ts) = rate t + complexity ts
+rate :: [Token] -- ^ tokens
+     -> Integer -- ^ complexity
+rate ts = foldr (\t s -> s + complexity t) 0 ts
   where
-    rate (Plain _)    = 0
-    rate (Question _) = 1
-    rate Asterisk     = 5
+    complexity (Plain _)    = 0
+    complexity (Question _) = 1
+    complexity Asterisk     = 5
 
 -- | Find out if all elements of a list are the same.
 same :: (Eq a)
      => [a]  -- ^ list
      -> Bool -- ^ decision
-same []       = True
-same [_]      = True
-same (x:y:zs) = x == y && same (y:zs)
+same xs = and $ zipWith (==) xs (tail xs)
 
