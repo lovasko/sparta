da3c8f8ae8d73dcd688f08e9247eb8528cfde0ae
Daniel Lovasko <daniel.lovasko@gmail.com>
2017-01-23 22:56:10 +0000

    wip on the main program

--- src/cli/Main.hs
+++ src/cli/Main.hs
@@ -1,3 +1,73 @@
-main :: IO ()
-main = putStrLn "Not implemented yet."
+{-# LANGUAGE OverloadedStrings #-}
 
+import Data.Maybe
+import Options.Applicative
+import System.Exit
+import Text.Comma
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
+import qualified Sparta as S
+
+import Options
+
+
+-- | Create named columns. In the case that there are no table headers
+-- provided, strings "1", "2", ... are used.
+columnNames :: Maybe [T.Text]   -- ^ table headers
+            -> Int              -- ^ column count
+            -> [(T.Text,  Int)] -- ^ column header dictionary
+columnNames hdrs len = zip keys [0..len-1]
+  where keys = fromMaybe (map (T.pack . show) [0..len-1]) hdrs
+
+-- | Split the full query string into the column and value parts based on the
+-- position of the '=' character. The first occurrence of '=' is used as the
+-- breaking point.
+splitQuery :: T.Text           -- ^ query
+           -> (T.Text, T.Text) -- ^ column & value
+splitQuery query
+  | length parts > 2  = (head parts, T.intercalate "=" (tail parts))
+  | length parts == 1 = (head parts, T.empty)
+  | otherwise         = (T.empty, T.empty)
+  where parts = T.split (== '=') query 
+
+-- | Parse a single query argument.
+parseQuery :: [(T.Text, Int)]             -- ^ column header dictionary
+           -> T.Text                      -- ^ query
+           -> Either String (Int, T.Text) -- ^ error message | query
+parseQuery dict query
+  | T.null query         = Left "No query specified"
+  | T.all (/= '=') query = Left "Delimiter '=' missing"
+  | T.null col           = Left "No column specified"
+  | T.null val           = Left "No value specified"
+  | isNothing idx        = Left "No such column"
+  | otherwise            = Right (fromJust idx, val)
+  where
+    (col, val) = splitQuery query
+    idx        = lookup dict col
+
+-- | Perform a table search based on the command-line options.
+search :: Options                  -- ^ command-line options
+       -> S.Table                  -- ^ table
+       -> Either String [[T.Text]] -- ^ error | result
+search options table = S.search table queries
+  where
+    queries = map (parseQuery columns) (optQueries options)
+    columns = columnNames headers (length $ head table)
+    headers = if optNoHeader options then Nothing else Just (head table)
+
+-- | Read the input file text. In case that no file was specified, the 
+-- standard input is used.
+getInput :: Options   -- ^ command-line options
+         -> IO T.Text -- ^ input
+getInput options = case optFile options of
+  Just file -> T.readFile file
+  Nothing   -> T.getContents
+
+-- | Sparse table query engine.
+main :: IO ()
+main = do
+  options <- execParser Options.parser
+  input   <- getInput options
+  case comma input >>= S.build >>= search of
+    Left err  -> T.putStrLn ("ERROR: " <> T.pack err) >> exitFailure
+    Right res -> T.putStrLn (uncomma res)             >> exitSuccess
